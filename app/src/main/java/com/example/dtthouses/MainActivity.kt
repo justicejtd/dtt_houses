957a7c0545d1375e44d8f969da7c1a30c7780c87
Justice Dreischor <t-justice98@hotmail.com>
2021-12-26 18:14:17 +0100

    Update MainActivity.kt
    bottom navigation setup

--- app/src/main/java/com/example/dtthouses/MainActivity.kt
+++ app/src/main/java/com/example/dtthouses/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.dtthouses
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Test pull request
+
+        // Initialize views
+        val host: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerViewHost) as NavHostFragment
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        // Setup navigation graph controller to bottom navigation
+        bottomNav?.setupWithNavController(host.navController)
     }
 }

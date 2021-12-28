6a3caa84e1ce9fb899444c4013a5ce888521cdf7
Justice Dreischor <t-justice98@hotmail.com>
2021-12-28 18:46:16 +0100

    added filter function

--- app/src/main/java/com/example/dtthouses/ui/houseOverview/HouseViewModel.kt
+++ app/src/main/java/com/example/dtthouses/ui/houseOverview/HouseViewModel.kt
@@ -12,12 +12,14 @@ import kotlin.collections.ArrayList
 
 class HouseViewModel(private val repository: MainRepository) : ViewModel() {
     private val housesLiveData = MutableLiveData<Resource<ArrayList<House>>>()
+    private lateinit var houses: ArrayList<House>
+    private val isSearchNotFound = MutableLiveData<Boolean>(false)
 
     init {
         // Get houses from repository
         viewModelScope.launch {
             housesLiveData.postValue(Resource.loading(null))
-            val houses = repository.getHouses()
+            houses = repository.getHouses()
             if (houses.size == 0) {
                 // If something goes wrong notify view with an error message
                 housesLiveData.postValue(Resource.error("No houses found!", null))
@@ -33,5 +35,43 @@ class HouseViewModel(private val repository: MainRepository) : ViewModel() {
 
     fun getHouses(): LiveData<Resource<ArrayList<House>>> {
         return housesLiveData
+    }
+
+    fun filterCourseListBySearch(input: String?) {
+        val filteredHouses = houses.filter { house ->
+            // Make a search pattern with combination of city and/or zip code
+            var isFound = isFoundQueryFound(input.toString(), house.city, house.zip)
+            if (!isFound) {
+                // Make a search pattern with combination of zip code and/or city
+                isFound = isFoundQueryFound(input.toString(), house.zip, house.city)
+
+                // If search is not found then show search not found image
+                if (!isFound) {
+                    isSearchNotFound.value = true
+                }
+            } else {
+                isSearchNotFound.value = false
+            }
+            isFound
+        }
+        housesLiveData.postValue(Resource.success(ArrayList(filteredHouses)))
+    }
+
+    private fun isFoundQueryFound(input: String, value1: String, value2: String): Boolean {
+        // Make a search pattern with combination of city and/or postal code
+        var pattern = value1.uppercase().plus(value2.uppercase())
+
+        // Remove any white spaces
+        pattern = pattern.replace("\\s".toRegex(), "")
+
+        // Set input value to uppercase and remove any whitespaces
+        val query = input.uppercase().replace("\\s".toRegex(), "")
+
+        // Check if house is found
+        return pattern.contains(query)
+    }
+
+    fun getIsSearchNotFound(): LiveData<Boolean> {
+        return isSearchNotFound
     }
 }

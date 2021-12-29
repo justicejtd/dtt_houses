de8ab78a22c9b8fb5edf185544554c983aa254bd
Justice Dreischor <t-justice98@hotmail.com>
2021-12-29 03:04:11 +0100

    added about fragment

--- app/src/main/java/com/example/dtthouses/ui/about/AboutFragment.kt
+++ app/src/main/java/com/example/dtthouses/ui/about/AboutFragment.kt
@@ -5,12 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.text.method.LinkMovementMethod
+import android.content.Context
+import android.widget.TextView
+import android.text.Spanned
+import android.text.TextPaint
+import android.graphics.Color
+import android.text.style.ClickableSpan
+import android.text.SpannableString
 import com.example.dtthouses.R
+import java.lang.RuntimeException
+
 
 class AboutFragment : Fragment() {
 
+    private lateinit var aboutFragmentCallback: AboutFragmentCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is AboutFragmentCallback) {
+            aboutFragmentCallback = context
+        } else {
+            throw RuntimeException(context.toString().plus(" must implement AboutFragmentCallback"))
+        }
     }
 
     override fun onCreateView(
@@ -18,6 +39,31 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        // Sett clickable link
+        setDTTLink(view)
+
+        return view
+    }
+
+    private fun setDTTLink(view: View) {
+        val ss = SpannableString("d-tt.nl")
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(textView: View) {
+                aboutFragmentCallback.showBrowser()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+            }
+        }
+        ss.setSpan(clickableSpan, 0, 7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val textView = view.findViewById<TextView>(R.id.tvLink)
+        textView.text = ss
+        textView.movementMethod = LinkMovementMethod.getInstance()
+        textView.highlightColor = Color.TRANSPARENT
     }
 }

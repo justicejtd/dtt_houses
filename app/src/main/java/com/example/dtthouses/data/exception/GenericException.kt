cbad5e506be577a9bf21043836d779dd3362213b
Justice Dreischor <t-justice98@hotmail.com>
2022-10-31 19:56:35 +0100

    feat: Added Generic to handle generic exceptions

---
+++ app/src/main/java/com/example/dtthouses/data/exception/GenericException.kt
@@ -0,0 +1,19 @@
+package com.example.dtthouses.data.exception
+
+import com.example.dtthouses.data.contstant.Error.EXCEPTION_ERROR_PREFIX
+
+/**
+ * This is a generic exception used to handle unexpected errors
+ */
+class GenericException : Exception(GENERIC_ERROR_MESSAGE) {
+
+    /**
+     * Generic exception constant values.
+     */
+    companion object {
+        /**
+         * Generic error message when something goes wrong.
+         */
+        private const val GENERIC_ERROR_MESSAGE = "$EXCEPTION_ERROR_PREFIX An unexpected error has occurred, please contact DTT support"
+    }
+}

27deb243d98cc481cd2b1f20fe3ad641a84dca79
Justice Dreischor <t-justice98@hotmail.com>
2022-10-31 19:58:31 +0100

    feat: Added response and error handling to HttpHouseRepoImpl

---
+++ app/src/main/java/com/example/dtthouses/data/api/repository/house/httpHouse/HttpHouseRepo.kt
@@ -0,0 +1,16 @@
+package com.example.dtthouses.data.api.repository.house.httpHouse
+
+import com.example.dtthouses.data.exception.GenericException
+import com.example.dtthouses.data.exception.NetworkException
+import com.example.dtthouses.data.model.House
+
+/**
+ * House repository call house data from house service
+ */
+interface HttpHouseRepo {
+    /**
+     * Gets a list of houses from API.
+     * @return Returns list of [House]
+     */
+    suspend fun getHouses(): List<House>
+}
---
+++ app/src/main/java/com/example/dtthouses/data/api/repository/house/httpHouse/HttpHouseRepoImpl.kt
@@ -0,0 +1,30 @@
+package com.example.dtthouses.data.api.repository.house.httpHouse
+
+import com.example.dtthouses.data.api.service.house.HouseService
+import com.example.dtthouses.data.exception.GenericException
+import com.example.dtthouses.data.exception.NetworkException
+import com.example.dtthouses.data.model.House
+import retrofit2.Response
+
+/**
+ * House repository makes network calls from [HouseService].
+ */
+class HttpHouseRepoImpl(private val service: HouseService) : HttpHouseRepo {
+
+    @Throws(NetworkException::class, GenericException::class)
+    override suspend fun getHouses(): List<House> {
+        val response: Response<List<House>>
+
+        try {
+             response = service.getHouses()
+        } catch (ex: Exception) {
+            throw GenericException()
+        }
+
+        // Throw network exception if something goes during network call.
+        if (response.isSuccessful.not()) throw NetworkException()
+
+        // Returns houses or throws generic exception if houses is equals to null.
+        return response.body() ?: throw GenericException()
+    }
+}

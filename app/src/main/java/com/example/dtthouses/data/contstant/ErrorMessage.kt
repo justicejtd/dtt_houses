39523b462f61f14649ea2db635ef43db86475fbc
justice6 <t-justice98@hotmail.com>
2022-11-03 03:27:23 +0100

    refactor: Removed unused code and imports

--- app/src/main/java/com/example/dtthouses/data/contstant/ErrorMessage.kt
+++ app/src/main/java/com/example/dtthouses/data/contstant/ErrorMessage.kt
@@ -8,9 +8,4 @@ object ErrorMessage {
      * Exception prefix when there is an error during network calls
      */
     const val EXCEPTION_ERROR_PREFIX = "Error:"
-
-    /**
-     * Exception message when view model is not found during initialization.
-     */
-    const val FACTORY_EXCEPTION_MSG = "Unknown ViewModel"
 }

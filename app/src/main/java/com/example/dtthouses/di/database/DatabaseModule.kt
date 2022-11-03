f6e635bc3e901cf1c0f6a01da6bba4efe5e735be
justice6 <t-justice98@hotmail.com>
2022-11-03 03:29:07 +0100

    feat: Added dependency modules
    UseCasesModuel, ServiceModule, RepsitoryModule and DatabaseModule has been added.

---
+++ app/src/main/java/com/example/dtthouses/di/database/DatabaseModule.kt
@@ -0,0 +1,34 @@
+package com.example.dtthouses.di.database
+
+import android.app.Application
+import com.example.dtthouses.data.database.AppDatabase
+import com.example.dtthouses.data.database.dao.HouseDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Database module to inject database and dao's instances.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    /**
+     * Provides [AppDatabase] instance.
+     */
+    @Provides
+    @Singleton
+    fun providesDatabase(application: Application): AppDatabase = AppDatabase.getInstance(application)
+
+
+    /**
+     * Provides [HouseDao] instance.
+     */
+    @Provides
+    @Singleton
+    fun providesHouseDao(appDatabase: AppDatabase): HouseDao = appDatabase.getHouseDao()
+
+}

fcf11aecea1aec7fdf5abf05a2eacc0fca070e96
justice6 <t-justice98@hotmail.com>
2022-11-17 19:42:36 +0100

    refactor: Moved showBrowser function from HomeActivity to AboutFragment

--- app/src/main/java/com/example/dtthouses/ui/about/AboutFragment.kt
+++ app/src/main/java/com/example/dtthouses/ui/about/AboutFragment.kt
@@ -6,28 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.text.method.LinkMovementMethod
-import android.content.Context
+import android.content.Intent
 import android.widget.TextView
 import android.text.Spanned
 import android.text.TextPaint
 import android.graphics.Color
+import android.net.Uri
 import android.text.style.ClickableSpan
 import android.text.SpannableString
 import com.example.dtthouses.R
-import com.example.dtthouses.ui.about.AboutFragment.AboutFragmentConstants.SPAN_DTT_LINK_END
-import com.example.dtthouses.ui.about.AboutFragment.AboutFragmentConstants.SPAN_DTT_LINK_START
-import java.lang.RuntimeException
 
 /**
  * Fragment for showing information of DTT.
  */
 class AboutFragment : Fragment() {
-    private lateinit var aboutFragmentCallback: AboutFragmentCallback
 
     /**
      * Constants values of AboutFragment.
      */
-    object AboutFragmentConstants {
+    companion object {
         /**
          * Span start value for DTT link name.
          */
@@ -37,23 +34,16 @@ class AboutFragment : Fragment() {
          * Span end value for DTT link name.
          */
         const val SPAN_DTT_LINK_END = 7
-    }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is AboutFragmentCallback) {
-            aboutFragmentCallback = context
-        } else {
-            throw RuntimeException(
-                context.toString().plus(" ")
-                    .plus(getString(R.string.about_fragment_runtime_exception))
-            )
-        }
+        /**
+         * URL link of DTT main website.
+         */
+        private const val DTT_URL = "https://www.d-tt.nl/"
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_about, container, false)
@@ -67,9 +57,7 @@ class AboutFragment : Fragment() {
     private fun setDTTLink(view: View) {
         val ss = SpannableString(getString(R.string.dtt_link_name))
         val clickableSpan: ClickableSpan = object : ClickableSpan() {
-            override fun onClick(textView: View) {
-                aboutFragmentCallback.showBrowser()
-            }
+            override fun onClick(textView: View) = showBrowser()
 
             override fun updateDrawState(ds: TextPaint) {
                 super.updateDrawState(ds)
@@ -87,5 +75,11 @@ class AboutFragment : Fragment() {
         textView.text = ss
         textView.movementMethod = LinkMovementMethod.getInstance()
         textView.highlightColor = Color.TRANSPARENT
+    }
+
+    private fun showBrowser() {
+        val intent =
+            Intent(Intent.ACTION_VIEW, Uri.parse(DTT_URL))
+        startActivity(intent)
     }
 }
